0f116873126c2e50259fbb3fdfca541efb509b74
Christopher Aue <mail@christopheraue.net>
2017-05-03 12:33:31 +0200

    merged tests for FiberedEventLoop with the ones for IOEventLoop

--- spec/io_event_loop_spec.rb
+++ spec/io_event_loop_spec.rb
@@ -6,8 +6,10 @@ describe IOEventLoop do
   describe "#start" do
     subject { instance.start }
 
+    before { instance.once{ expect(instance).to be_running } }
+    after { expect(instance).not_to be_running }
+
     context "when it has no timers and nothing to watch" do
-      before { expect(instance).to receive(:stop).and_call_original }
       it { is_expected.to be nil }
     end
 
@@ -16,7 +18,6 @@ describe IOEventLoop do
       let(:callback) { proc{} }
       before { expect(callback).to receive(:call) }
 
-      before { expect(instance).to receive(:stop).and_call_original }
       it { is_expected.to be nil }
     end
 
@@ -37,11 +38,165 @@ describe IOEventLoop do
         before { instance.await_writable(writer) }
 
         it { is_expected.to be nil }
+
         after do
           writer.write 'Hello!'; writer.close
           expect(reader.read).to eq 'Hello!'
         end
       end
+    end
+  end
+
+  describe "an iteration causing an error" do
+    subject { instance.start }
+
+    before { instance.once{ raise 'evil error' } }
+
+    before { expect(instance).to receive(:trigger).with(:error,
+      (be_a(RuntimeError).and have_attributes message: 'evil error')).and_call_original }
+
+    context "when the loop should stop and raise the error (the default)" do
+      it { is_expected.to raise_error 'evil error' }
+    end
+
+    context "when the loop should forgive the error" do
+      before { instance.forgive_iteration_errors! }
+      it { is_expected.to be nil }
+    end
+  end
+
+  describe "#await, #awaits? and #resume" do
+    context "when waiting originates from the root fiber" do
+      before { instance.once{ instance.resume(:request, :result) } }
+      it { expect(instance.await(:request)).to be :result }
+    end
+
+    context "when waiting originates from a fiber" do
+      # The loop has to be started first to enter a fiber
+      subject { instance.start }
+
+      before { instance.once{ @result = instance.await(:request) } }
+      before { instance.once{ instance.await(:another_request) } }
+      before { instance.once{ instance.resume(:request, :result) } }
+
+      it { is_expected.to be nil }
+      after { expect(@result).to be :result }
+      after { expect(instance.awaits? :request).to be false }
+      after { expect(instance.awaits? :another_request).to be true }
+    end
+
+    context "when resuming a fiber with an unknown id" do
+      subject { instance.resume(:unknown, :result) }
+      it { is_expected.to raise_error IOEventLoop::UnknownWaitingIdError, "unknown waiting id :unknown" }
+    end
+
+    context "when resuming a fiber raises an error" do
+      subject { instance.start }
+
+      # e.g. resuming the fiber raises a FiberError
+      before { instance.once do
+        allow(Fiber.current).to receive(:resume).and_raise FiberError, 'resume error'
+        instance.await(:request)
+      end }
+      before { instance.once{ instance.resume(:request, :result) } }
+
+      it { is_expected.to raise_error IOEventLoop::CancelledError, 'resume error' }
+    end
+
+    context "when #await is given a timeout" do
+      subject { instance.await(:id, within: 0.02, timeout_result: timeout_result) }
+
+      let(:timeout_result) { :timeout_result }
+
+      context "when the result arrives in time" do
+        before { instance.timers.after(0.01) { instance.resume(:id, :result) } }
+        it { is_expected.to be :result }
+      end
+
+      context "when evaluation of result is too slow" do
+        context "when the timeout result is a timeout error" do
+          let(:timeout_result) { IOEventLoop::TimeoutError.new("Time's up!") }
+          it { is_expected.to raise_error IOEventLoop::TimeoutError, "Time's up!" }
+        end
+
+        context "when the timeout result is not an timeout error" do
+          let(:timeout_result) { :timeout_result }
+          it { is_expected.to be :timeout_result }
+        end
+      end
+    end
+  end
+
+  describe "#cancel" do
+    context "when cancelling the root fiber" do
+      subject { instance.await(:request) }
+      
+      before { instance.once{ instance.cancel(:request, *reason) } }
+      let(:reason) { nil }
+      it { is_expected.to raise_error IOEventLoop::CancelledError, "waiting for id :request cancelled" }
+
+      context "when giving a reason" do
+        let(:reason) { 'cancel reason' }
+        it { is_expected.to raise_error IOEventLoop::CancelledError, "cancel reason" }
+      end
+    end
+
+    context "when cancelling an iteration fiber" do
+      # The loop has to be started first to enter a fiber
+      subject { instance.start }
+
+      before { instance.once do
+        begin
+          instance.await(:request)
+        rescue IOEventLoop::CancelledError => e
+          @result = e
+        end
+      end }
+      before { instance.once{ @cancel_result = instance.cancel(:request, *reason) } }
+      let(:reason) { nil }
+
+      context "when giving no explicit reason" do
+        it { is_expected.to be nil }
+        after { expect(@result).to be_a(IOEventLoop::CancelledError).and having_attributes(
+          message: "waiting for id :request cancelled") }
+        after { expect(@cancel_result).to be :cancelled }
+      end
+
+      context "when giving a reason" do
+        let(:reason) { 'cancel reason' }
+
+        it { is_expected.to be nil }
+        after { expect(@result).to be_a(IOEventLoop::CancelledError).and having_attributes(
+          message: "cancel reason") }
+        after { expect(@cancel_result).to be :cancelled }
+      end
+    end
+
+    context "when cancelling a fiber with an unknown id" do
+      subject { instance.cancel(:unknown) }
+      it { is_expected.to raise_error IOEventLoop::UnknownWaitingIdError, "unknown waiting id :unknown" }
+    end
+  end
+
+  describe "#once" do
+    subject { instance.start }
+
+    context "when the once block executes no request of its own" do
+      before { instance.once{ @once_ran1 = 1 } }
+      before { instance.once{ @once_ran2 = 2 } }
+
+      it { is_expected.to be nil }
+      after { expect(@once_ran1).to be 1 }
+      after { expect(@once_ran2).to be 2 }
+    end
+
+    context "when the once block itself waits for a request" do
+      before { instance.once{ @once_result = instance.await(:request) } }
+      before { instance.once{ :do_nothing } }
+      before { instance.once{ instance.resume(:request, :result) } }
+
+      it { is_expected.to be nil }
+      after { expect(@once_result).to be :result }
     end
   end
 
@@ -77,19 +232,6 @@ describe IOEventLoop do
 
       before { expect(callback1).to receive(:call).and_call_original }
       it { is_expected.to be nil }
-    end
-  end
-
-  describe "#await with timeout" do
-    subject { instance.await(:id, within: 0.02, timeout_result: IOEventLoop::TimeoutError.new("Time's up!")) }
-
-    context "when the result arrives in time" do
-      before { instance.timers.after(0.01) { instance.resume(:id, :result) } }
-      it { is_expected.to be :result }
-    end
-
-    context "when evaluation of result is too slow" do
-      it { is_expected.to raise_error IOEventLoop::TimeoutError, "Time's up!" }
     end
   end
 
@@ -186,5 +328,14 @@ describe IOEventLoop do
         it { is_expected.to raise_error IOEventLoop::TimeoutError, "Time's up!" }
       end
     end
+  end
+
+  describe "#watch_events" do
+    subject { instance.watch_events(object, :event) }
+
+    let(:object) { Object.new.extend CallbacksAttachable }
+
+    it { is_expected.to be_a(IOEventLoop::EventWatcher).and having_attributes(loop: instance,
+      subject: object, event: :event)}
   end
 end
