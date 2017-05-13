55021f0ac8ebaf28ab5c7fb6996d05c6220a2db8
Christopher Aue <mail@christopheraue.net>
2017-05-13 02:30:03 +0200

    added a fast track for carts being scheduled immediately

--- mrblib/io_event_loop_run_queue.rb
+++ mrblib/io_event_loop_run_queue.rb
@@ -4,13 +4,18 @@ class IOEventLoop
       @wall_clock = WallClock.new
       @cart_pool = CartPool.new
       @cart_track = []
+      @fast_track = []
     end
 
     def schedule(fiber, seconds, result = nil)
-      time = @wall_clock.now+seconds
-      cart = @cart_pool.take_and_load_with(fiber, time, result)
-      index = @cart_track.bisect_left{ |cart| cart.time <= time }
-      @cart_track.insert(index, cart)
+      if seconds == 0
+        @fast_track.push @cart_pool.take_and_load_with(fiber, nil, result)
+      else
+        time = @wall_clock.now+seconds
+        cart = @cart_pool.take_and_load_with(fiber, time, result)
+        index = @cart_track.bisect_left{ |cart| cart.time <= time }
+        @cart_track.insert(index, cart)
+      end
     end
 
     def cancel(fiber)
@@ -18,13 +23,21 @@ class IOEventLoop
     end
 
     def process_pending
-      now = @wall_clock.now
-      index = @cart_track.bisect_left{ |cart| cart.time <= now }
-      @cart_track.pop(@cart_track.length-index).reverse_each(&:unload_and_process)
+      processing = @fast_track
+      @fast_track = []
+      processing.each(&:unload_and_process)
+
+      if @cart_track.any?
+        now = @wall_clock.now
+        index = @cart_track.bisect_left{ |cart| cart.time <= now }
+        @cart_track.pop(@cart_track.length-index).reverse_each(&:unload_and_process)
+      end
     end
 
     def waiting_time
-      if next_scheduled = @cart_track.reverse_each.find(&:loaded?)
+      if @fast_track.any?
+        0
+      elsif next_scheduled = @cart_track.reverse_each.find(&:loaded?)
         waiting_time = next_scheduled.time - @wall_clock.now
         waiting_time < 0 ? 0 : waiting_time
       end
