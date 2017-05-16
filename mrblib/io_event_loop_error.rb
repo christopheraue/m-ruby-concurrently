7233bfdd3f14adb3ae851e7e28419528057e571a
Christopher Aue <mail@christopheraue.net>
2017-05-16 22:43:18 +0200

    made CancelledConcurrentBlock an Exception

--- mrblib/io_event_loop_error.rb
+++ mrblib/io_event_loop_error.rb
@@ -3,7 +3,9 @@ class IOEventLoop
   class CancelledError < Error; end
   class TimeoutError < CancelledError; end
   class EventWatcherError < Error; end
-  class CancelledConcurrentBlock < Error; end
+
+  # should not be rescued accidentally and therefore is an exception
+  class CancelledConcurrentBlock < Exception; end
 
   class ConcurrentEvaluation
     Error = IOEventLoop::Error
