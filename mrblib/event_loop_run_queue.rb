d8882a98695bbe675934870991f87ed09b3e7397
Christopher Aue <mail@christopheraue.net>
2017-05-19 16:27:10 +0200

    used FIBER field of run queue carts to mark carts as (in)active

--- mrblib/event_loop_run_queue.rb
+++ mrblib/event_loop_run_queue.rb
@@ -1,8 +1,8 @@
 module Concurrently
   class EventLoop::RunQueue
     # The items of the run queue are called carts. Carts are simple arrays
-    # with the following layout: [active, fiber, time, result]
-    ACTIVE = 0; FIBER = 1; TIME = 2; RESULT = 3
+    # with the following layout: [fiber, time, result]
+    FIBER = 0; TIME = 1; RESULT = 2
 
     # There are two tracks. The fast track and the regular cart track. The
     # fast track exists for fibers to be scheduled immediately. Having a
@@ -11,7 +11,7 @@ module Concurrently
     # on the regular cart track to insert the cart.
 
     # The additional cart index exists so carts can be cancelled by their
-    # fiber.
+    # fiber. Cancelled carts have their fiber set to false.
 
     def initialize(loop)
       @loop = loop
@@ -21,21 +21,21 @@ module Concurrently
     end
 
     def schedule_now(fiber, result = nil)
-      cart = [true, fiber, nil, result]
+      cart = [fiber, nil, result]
       @cart_index[fiber.hash] = cart
       @fast_track << cart
     end
 
     def schedule(fiber, seconds, result = nil)
-      cart = [true, fiber, @loop.lifetime+seconds, result]
+      cart = [fiber, @loop.lifetime+seconds, result]
       @cart_index[fiber.hash] = cart
       index = @cart_track.bisect_left{ |cart_on_track| cart_on_track[TIME] <= cart[TIME] }
       @cart_track.insert(index, cart)
     end
 
     def cancel(fiber)
-      if cart = @cart_index[fiber.hash]
-        cart[ACTIVE] = false
+      if cart = @cart_index.delete(fiber.hash)
+        cart[FIBER] = false
       end
     end
 
@@ -56,7 +56,7 @@ module Concurrently
       processing.each do |cart|
         begin
           @cart_index.delete cart[FIBER].hash
-          cart[FIBER].send_to_foreground! cart[RESULT] if cart[ACTIVE]
+          cart[FIBER].send_to_foreground! cart[RESULT] if cart[FIBER]
         rescue => error
           # Keep loop up and running but trigger a callback so the user can
           # do something in an error case.
@@ -68,7 +68,7 @@ module Concurrently
     def waiting_time
       if @fast_track.any?
         0
-      elsif next_cart = @cart_track.reverse_each.find{ |cart| cart[ACTIVE] }
+      elsif next_cart = @cart_track.reverse_each.find{ |cart| cart[FIBER] }
         waiting_time = next_cart[TIME] - @loop.lifetime
         waiting_time < 0 ? 0 : waiting_time
       end
