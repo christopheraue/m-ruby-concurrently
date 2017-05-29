fd1deede193c7afa58130324e1724231474ae6ab
Christopher Aue <mail@christopheraue.net>
2017-05-29 23:03:23 +0200

    documented IO#await_readable

--- mrblib/_ext_io.rb
+++ mrblib/_ext_io.rb
@@ -5,7 +5,40 @@
 #
 # @api public
 class IO
-  # Suspends the current concurrent proc or fiber until IO is readable
+  # Suspends the current concurrent proc until IO is readable. It can also be
+  # used outside of concurrent procs.
+  #
+  # While waiting, the code jumps to the event loop and executes other
+  # concurrent procs that are ready to run in the meantime.
+  #
+  # @example Waiting inside a concurrent proc
+  #   r,w = IO.pipe
+  #
+  #   wait_proc = concurrent_proc do
+  #      r.await_readable
+  #      r.read 24 # 'Hey from the other proc!' has 24 bytes
+  #   end
+  #
+  #   concurrently do
+  #     w.write 'Hey from the other proc!'
+  #   end
+  #
+  #   wait_proc.call # => 'Hey from the other proc!'
+  #
+  #   r.close; w.close
+  #
+  # @example Waiting outside a concurrent proc
+  #   r,w = IO.pipe
+  #
+  #   concurrently do
+  #     puts "I'm running while the outside is waiting!"
+  #     w.write "Continue!"
+  #   end
+  #
+  #   r.await_readable
+  #
+  #   # prints: "I'm running while the outside is waiting!"
+  #   # then continues since r is readable again
   def await_readable(opts = {})
     io_watcher = Concurrently::EventLoop.current.io_watcher
     fiber = Fiber.current
