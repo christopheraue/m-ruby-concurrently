abbd34a43b1019fc94667af24636fa4e2572c63b
Christopher Aue <mail@christopheraue.net>
2017-06-03 12:56:49 +0200

    renamed file with array extensions for mruby

--- mrblib/_ext_array_m.rb
+++ mrblib/_ext_array_m.rb
@@ -1,6 +1,6 @@
+# @api mruby
 class Array
   unless method_defined? :bsearch_index
-    # @api mruby
     # mruby does not implement Array#bsearch_index
     def bsearch_index
       # adapted from https://github.com/python-git/python/blob/7e145963cd67c357fcc2e0c6aca19bc6ec9e64bb/Lib/bisect.py#L67
