f56a337908dbfdd642f150d9b4e103508aaed881
Christopher Aue <mail@christopheraue.net>
2017-06-06 21:39:35 +0200

    added Array#bsearch_index shim for Ruby 2.2

--- mrblib/_ext_array_m.rb
+++ mrblib/_ext_array_m.rb
@@ -1,28 +1,6 @@
 # @api mruby_patches
 # @since 1.0.0
 class Array
-  unless method_defined? :bsearch_index
-    # Implements Array#bsearch_index for mruby.
-    def bsearch_index
-      # adapted from https://github.com/python-git/python/blob/7e145963cd67c357fcc2e0c6aca19bc6ec9e64bb/Lib/bisect.py#L67
-      len = length
-      lo = 0
-      hi = len
-
-      while lo < hi
-        mid = (lo + hi).div(2)
-
-        if yield self[mid]
-          hi = mid
-        else
-          lo = mid + 1
-        end
-      end
-
-      lo == len ? nil : lo
-    end
-  end
-
   # Alias for original Array#pop
   alias_method :pop_single, :pop
 
