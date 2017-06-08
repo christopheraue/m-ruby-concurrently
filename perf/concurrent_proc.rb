861634e7f7868f3fb27cbfd0587f47fd0e8bc4cf
Christopher Aue <mail@christopheraue.net>
2017-06-08 21:22:56 +0200

    added a regular proc to the comparison benchmark

--- perf/concurrent_proc.rb
+++ perf/concurrent_proc.rb
@@ -4,28 +4,38 @@ require_relative "_shared/stage"
 
 stage = Stage.new
 
+proc = proc{}
 conproc = concurrent_proc{}
+
+puts "proc = proc{}"
+puts "conproc = concurrent_proc{}"
+puts
+
+result = stage.measure(seconds: 1, profiler: RubyProf::FlatPrinter) do
+  proc.call
+end
+puts "proc.call:               #{result[:iterations]} iterations executed in #{result[:time].round 4} seconds"
 
 result = stage.measure(seconds: 1, profiler: RubyProf::FlatPrinter) do
   conproc.call
 end
 
-puts "           #call: #{result[:iterations]} iterations executed in #{result[:time].round 4} seconds"
+puts "conproc.call:            #{result[:iterations]}  iterations executed in #{result[:time].round 4} seconds"
 
 result = stage.measure(seconds: 1, profiler: RubyProf::FlatPrinter) do
   conproc.call_nonblock
 end
 
-puts "  #call_nonblock: #{result[:iterations]} iterations executed in #{result[:time].round 4} seconds"
+puts "conproc.call_nonblock:   #{result[:iterations]}  iterations executed in #{result[:time].round 4} seconds"
 
 result = stage.measure(seconds: 1, profiler: RubyProf::FlatPrinter) do
   conproc.call_detached
 end
 
-puts "  #call_detached: #{result[:iterations]} iterations executed in #{result[:time].round 4} seconds"
+puts "conproc.call_detached:   #{result[:iterations]}  iterations executed in #{result[:time].round 4} seconds"
 
 result = stage.measure(seconds: 1, profiler: RubyProf::FlatPrinter) do
   conproc.call_and_forget
 end
 
-puts "#call_and_forget: #{result[:iterations]} iterations executed in #{result[:time].round 4} seconds"
+puts "conproc.call_and_forget: #{result[:iterations]}  iterations executed in #{result[:time].round 4} seconds"
