f625de9d9f198361582f94c504e09d06db0c3ed2
Christopher Aue <mail@christopheraue.net>
2017-06-22 15:41:24 +0200

    added configurable batch size to benchmark

--- perf/concurrent_proc_calls_awaiting.rb
+++ perf/concurrent_proc_calls_awaiting.rb
@@ -4,6 +4,7 @@ require_relative "_shared/stage"
 
 stage = Stage.new
 format = "  %-25s %7d executions in %2.4f seconds"
+factor = ARGV.fetch(0, 1).to_i
 
 puts <<-DOC
 Benchmarked Code
@@ -11,7 +12,7 @@ Benchmarked Code
   conproc = concurrent_proc{ wait 0 }
   
   while elapsed_seconds < 1
-    # CODE #
+    #{factor}.times{ # CODE # }
     wait 0 # to enter the event loop
   end
 
@@ -23,25 +24,25 @@ DOC
 conproc = concurrent_proc{ wait 0 }
 
 result = stage.measure(seconds: 1) do
-  conproc.call
+  factor.times{ conproc.call }
   # no need to enter the event loop manually. It already happens in #call
 end
-puts sprintf(format, "conproc.call:", result[:iterations], result[:time])
+puts sprintf(format, "conproc.call:", factor*result[:iterations], result[:time])
 
 result = stage.measure(seconds: 1) do
-  conproc.call_nonblock
+  factor.times{ conproc.call_nonblock }
   wait 0
 end
-puts sprintf(format, "conproc.call_nonblock:", result[:iterations], result[:time])
+puts sprintf(format, "conproc.call_nonblock:", factor*result[:iterations], result[:time])
 
 result = stage.measure(seconds: 1) do
-  conproc.call_detached
+  factor.times{ conproc.call_detached }
   wait 0
 end
-puts sprintf(format, "conproc.call_detached:", result[:iterations], result[:time])
+puts sprintf(format, "conproc.call_detached:", factor*result[:iterations], result[:time])
 
 result = stage.measure(seconds: 1) do
-  conproc.call_and_forget
+  factor.times{ conproc.call_and_forget }
   wait 0
 end
-puts sprintf(format, "conproc.call_and_forget:", result[:iterations], result[:time])
+puts sprintf(format, "conproc.call_and_forget:", factor*result[:iterations], result[:time])
