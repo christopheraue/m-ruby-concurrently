425476be4ff72b59c9ba9622d8e48c831609a2ad
Christopher Aue <mail@christopheraue.net>
2017-06-22 13:35:51 +0200

    made benchmark more meaningful

--- perf/concurrent_proc_calls_awaiting.rb
+++ perf/concurrent_proc_calls_awaiting.rb
@@ -8,13 +8,12 @@ format = "  %-25s %7d executions in %2.4f seconds"
 puts <<-DOC
 Benchmarked Code
 ----------------
-  evaluation = Concurrently::Evaluation.current
-  proc = proc{ evaluation.resume! }
-  conproc = concurrent_proc{ evaluation.resume! }
+  proc = proc{ wait 0 }
+  conproc = concurrent_proc{ wait 0 }
   
   while elapsed_seconds < 1
     # CODE #
-    await_resume!
+    wait 0
   end
 
 Results
@@ -22,36 +21,35 @@ Results
   # CODE #
 DOC
 
-evaluation = Concurrently::Evaluation.current
-proc = proc{ evaluation.resume! }
-conproc = concurrent_proc{ evaluation.resume! }
+proc = proc{ wait 0 }
+conproc = concurrent_proc{ wait 0 }
 
 result = stage.measure(seconds: 1, profiler: RubyProf::FlatPrinter) do
   proc.call
-  await_resume!
+  wait 0
 end
 puts sprintf(format, "proc.call:", result[:iterations], result[:time])
 
 result = stage.measure(seconds: 1, profiler: RubyProf::FlatPrinter) do
   conproc.call
-  await_resume!
+  wait 0
 end
 puts sprintf(format, "conproc.call:", result[:iterations], result[:time])
 
 result = stage.measure(seconds: 1, profiler: RubyProf::FlatPrinter) do
   conproc.call_nonblock
-  await_resume!
+  wait 0
 end
 puts sprintf(format, "conproc.call_nonblock:", result[:iterations], result[:time])
 
 result = stage.measure(seconds: 1, profiler: RubyProf::FlatPrinter) do
   conproc.call_detached
-  await_resume!
+  wait 0
 end
 puts sprintf(format, "conproc.call_detached:", result[:iterations], result[:time])
 
 result = stage.measure(seconds: 1, profiler: RubyProf::FlatPrinter) do
   conproc.call_and_forget
-  await_resume!
+  wait 0
 end
 puts sprintf(format, "conproc.call_and_forget:", result[:iterations], result[:time])
