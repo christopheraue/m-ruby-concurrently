2b8816efc9100c4b68751db79d097fd11a7d03b7
Christopher Aue <mail@christopheraue.net>
2017-06-22 14:54:00 +0200

    removed unnecessary configuration of profiler

--- perf/concurrent_proc_calls.rb
+++ perf/concurrent_proc_calls.rb
@@ -23,27 +23,27 @@ DOC
 proc = proc{}
 conproc = concurrent_proc{}
 
-result = stage.measure(seconds: 1, profiler: RubyProf::FlatPrinter) do
+result = stage.measure(seconds: 1) do
   proc.call
 end
 puts sprintf(format, "proc.call:", result[:iterations], result[:time])
 
-result = stage.measure(seconds: 1, profiler: RubyProf::FlatPrinter) do
+result = stage.measure(seconds: 1) do
   conproc.call
 end
 puts sprintf(format, "conproc.call:", result[:iterations], result[:time])
 
-result = stage.measure(seconds: 1, profiler: RubyProf::FlatPrinter) do
+result = stage.measure(seconds: 1) do
   conproc.call_nonblock
 end
 puts sprintf(format, "conproc.call_nonblock:", result[:iterations], result[:time])
 
-result = stage.measure(seconds: 1, profiler: RubyProf::FlatPrinter) do
+result = stage.measure(seconds: 1) do
   conproc.call_detached
 end
 puts sprintf(format, "conproc.call_detached:", result[:iterations], result[:time])
 
-result = stage.measure(seconds: 1, profiler: RubyProf::FlatPrinter) do
+result = stage.measure(seconds: 1) do
   conproc.call_and_forget
 end
 puts sprintf(format, "conproc.call_and_forget:", result[:iterations], result[:time])
--- perf/concurrent_proc_calls_awaiting.rb
+++ perf/concurrent_proc_calls_awaiting.rb
@@ -24,31 +24,31 @@ DOC
 proc = proc{ wait 0 }
 conproc = concurrent_proc{ wait 0 }
 
-result = stage.measure(seconds: 1, profiler: RubyProf::FlatPrinter) do
+result = stage.measure(seconds: 1) do
   proc.call
   wait 0
 end
 puts sprintf(format, "proc.call:", result[:iterations], result[:time])
 
-result = stage.measure(seconds: 1, profiler: RubyProf::FlatPrinter) do
+result = stage.measure(seconds: 1) do
   conproc.call
   wait 0
 end
 puts sprintf(format, "conproc.call:", result[:iterations], result[:time])
 
-result = stage.measure(seconds: 1, profiler: RubyProf::FlatPrinter) do
+result = stage.measure(seconds: 1) do
   conproc.call_nonblock
   wait 0
 end
 puts sprintf(format, "conproc.call_nonblock:", result[:iterations], result[:time])
 
-result = stage.measure(seconds: 1, profiler: RubyProf::FlatPrinter) do
+result = stage.measure(seconds: 1) do
   conproc.call_detached
   wait 0
 end
 puts sprintf(format, "conproc.call_detached:", result[:iterations], result[:time])
 
-result = stage.measure(seconds: 1, profiler: RubyProf::FlatPrinter) do
+result = stage.measure(seconds: 1) do
   conproc.call_and_forget
   wait 0
 end
