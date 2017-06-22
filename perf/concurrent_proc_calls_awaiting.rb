21878bf8fa0986aee3103e4f71f4dc2a6e077ac9
Christopher Aue <mail@christopheraue.net>
2017-06-22 14:55:27 +0200

    removed regular proc from benchmark

--- perf/concurrent_proc_calls_awaiting.rb
+++ perf/concurrent_proc_calls_awaiting.rb
@@ -8,12 +8,11 @@ format = "  %-25s %7d executions in %2.4f seconds"
 puts <<-DOC
 Benchmarked Code
 ----------------
-  proc = proc{ wait 0 }
   conproc = concurrent_proc{ wait 0 }
   
   while elapsed_seconds < 1
     # CODE #
-    wait 0
+    wait 0 # to enter the event loop
   end
 
 Results
@@ -21,18 +20,11 @@ Results
   # CODE #
 DOC
 
-proc = proc{ wait 0 }
 conproc = concurrent_proc{ wait 0 }
 
 result = stage.measure(seconds: 1) do
-  proc.call
-  wait 0
-end
-puts sprintf(format, "proc.call:", result[:iterations], result[:time])
-
-result = stage.measure(seconds: 1) do
   conproc.call
-  wait 0
+  # no need to enter the event loop manually. It already happens in #call
 end
 puts sprintf(format, "conproc.call:", result[:iterations], result[:time])
 
