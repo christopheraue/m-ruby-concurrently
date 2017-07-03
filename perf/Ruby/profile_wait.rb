21c638c7cd07baae47477b40c7c953169d5cee1a
Christopher Aue <mail@christopheraue.net>
2017-07-03 12:33:10 +0200

    added script to profile waiting

---
+++ perf/Ruby/profile_wait.rb
@@ -0,0 +1,14 @@
+#!/bin/env ruby
+
+require_relative "_shared/stage"
+
+stage = Stage.new
+
+conproc = concurrent_proc{ wait 0 }
+
+result = stage.profile(seconds: 1) do
+  conproc.call
+  wait 0
+end
+
+puts "#{result[:iterations]} executions in #{result[:time].round 4} seconds"
