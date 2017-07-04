d7c3fa070664f45cd4e176586b0ad5e0b35bf483
Christopher Aue <mail@christopheraue.net>
2017-07-04 20:03:02 +0200

    improved profiling Kernel#wait

--- perf/profile_wait.rb
+++ perf/profile_wait.rb
@@ -4,7 +4,6 @@ conproc = concurrent_proc{ wait 0 }
 
 result = stage.profile(seconds: 1) do
   conproc.call
-  wait 0
 end
 
 puts "#{result[:iterations]} executions in #{result[:time].round 4} seconds"
