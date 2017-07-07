9e2c7a4c0b01cb577b3e527bdbf88d1a505d6bcb
Christopher Aue <mail@christopheraue.net>
2017-07-08 00:02:48 +0200

    refactored profiling wait methods

--- perf/profile_await_readable.rb
+++ perf/profile_await_readable.rb
@@ -1,19 +1,10 @@
 stage = Stage.new
 
-conproc = concurrent_proc do |r,w|
-  begin
-    r.read_nonblock 1
-  rescue IO::WaitReadable
-    w.write '0'
-    r.await_readable
-    retry
-  end
-end
-
 r,w = IO.pipe
+w.write '0'
 
 result = stage.profile(seconds: 1) do
-  conproc.call r,w
+  r.await_readable
 end
 
 puts "#{result[:iterations]} executions in #{result[:time].round 4} seconds"
--- perf/profile_wait.rb
+++ perf/profile_wait.rb
@@ -1,9 +1,7 @@
 stage = Stage.new
 
-conproc = concurrent_proc{ wait 0 }
-
 result = stage.profile(seconds: 1) do
-  conproc.call
+  wait 0
 end
 
 puts "#{result[:iterations]} executions in #{result[:time].round 4} seconds"
