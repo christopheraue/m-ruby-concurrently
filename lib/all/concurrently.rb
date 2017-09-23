5f6a132ed139d594c8e6c7d36513eee3ddd76700
Christopher Aue <mail@christopheraue.net>
2017-09-23 18:27:21 +0200

    added file dedicated to defining the root module

---
+++ lib/all/concurrently.rb
@@ -0,0 +1,7 @@
+# @api public
+# @since 1.0.0
+#
+# The namespace this library lives in
+module Concurrently
+
+end
--- lib/all/concurrently/version.rb
+++ lib/all/concurrently/version.rb
@@ -1,7 +1,3 @@
-# @api public
-# @since 1.0.0
-#
-# The namespace this library lives in
 module Concurrently
   # The version
   VERSION = "1.1.1"
